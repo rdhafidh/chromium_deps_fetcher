e9fd39d526c7bb98e3a0aa617f6c6a694afd2a25
Ridwan Abdul Hafidh <ridwanabdulhafidh@gmail.com>
2018-06-26 20:12:22 +0700

    remove unused enums

--- repodeps.h
+++ repodeps.h
@@ -1,15 +1,7 @@
 #ifndef REPODEPS_H
 #define REPODEPS_H
 #include <QString>
-
-
-enum class FindRepoStatus{
-      OK_GOOD=0x2,
-      NOT_FOUND_ANY_REPO,
-      PARSING_DEPS_FAILED,
-     OTHER_INTERNAL_ERROR
-};
-
+ 
 struct  DEPSRepoFound{
         QString reponame;
         QString linkname;
